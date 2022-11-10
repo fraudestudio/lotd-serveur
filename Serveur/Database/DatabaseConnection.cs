8cc2c7884d998d0d5fcd39d6d75ab2e5934128a5
MateoFavard <mateo.favard@iut-dijon.u-bourgogne.fr>
2022-11-10 09:31:22 +0100

    Changement mysql mysqlconnector

--- Serveur/Database/DatabaseConnection.cs
+++ Serveur/Database/DatabaseConnection.cs
@@ -1,13 +1,19 @@
-using MySqlConnector;
+using System;
 using System.Data;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace Server.Database
 {
     static public class DatabaseConnection
     {
+        static private String _USERNAME = Environment.GetEnvironmentVariable("DATABASE_USERNAME") ?? "";
+        static private String _PASSWORD = Environment.GetEnvironmentVariable("DATABASE_PASSWORD") ?? "";
+
         static public MySqlConnection NewConnection()
         {
-            return new MySqlConnection("Server=127.0.0.1;User ID=fraude;Password=password;Port=3306;Database=bdd_lotd");
+            return new MySqlConnection($"Server=127.0.0.1;User ID={_USERNAME};Password={_PASSWORD};Port=3306;Database=bdd_lotd");
         }
     }
 }
--- Serveur/Database/Univers.cs
+++ Serveur/Database/Univers.cs
@@ -1,11 +1,14 @@
-using MySqlConnector;
-using Server.Utils;
-using Server.Database;
+using System;
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
 
 namespace Server.Database
 {
     static public class Univers
     {
+
 
     }
 }
