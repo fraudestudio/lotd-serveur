f0d0cd847868295c1f86cda212fb293945632b46
MarcGrandvincent <105056008+MarcGrandvincent@users.noreply.github.com>
2022-11-10 16:47:18 +0100

    CreateUniverse

--- Serveur/Controllers/Api/UniverseController.cs
+++ Serveur/Controllers/Api/UniverseController.cs
@@ -1,6 +1,41 @@
-﻿namespace Serveur.Controllers.Api
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Serveur.Database;
+using Serveur.Model;
+using System.Text.Json;
+
+namespace Serveur.Controllers.Api
 {
+    [ApiController]
+    [Route("api/universe")]
     public class UniverseController
     {
+
+        [HttpPost("createuniverse")]
+        public async Task<IActionResult> SignIn(
+            [FromForm] string name,
+            [FromForm] string password,
+            [FromForm] string owner)
+        {
+            string response = "";
+
+            if (await Universe.InsertUnivers(name, password, owner))
+            {
+                CreateUniverseSuccess data = new CreateUniverseSuccess("Universe create successfully");
+                response = JsonSerializer.Serialize<CreateUniverseSuccess>(data);
+            }
+            else
+            {
+                CreateUniverseFailure data = new CreateUniverseFailure("Universe could not be created");
+                response = JsonSerializer.Serialize<CreateUniverseFailure>(data);
+            }
+
+            var result = new ContentResult
+            {
+                Content = response,
+                ContentType = "application/json; charset=UTF-8",
+            };
+            return result;
+        }
     }
 }
---
+++ Serveur/Model/CreateUniverseFailure.cs
@@ -0,0 +1,14 @@
+﻿namespace Serveur.Model
+{
+    public class CreateUniverseFailure: CreateUniverse
+    {
+        private String _reason;
+
+        public CreateUniverseFailure(String reason) : base(false)
+        {
+            this._reason = reason;
+        }
+
+        public String Reason => this._reason;
+    }
+}
