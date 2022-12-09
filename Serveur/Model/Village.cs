b008b22d33665f86d44a678bfd7a87fd484d6068
MarcGrandvincent <105056008+MarcGrandvincent@users.noreply.github.com>
2022-12-09 09:07:42 +0100

    Modification warning

--- Serveur/Model/Village.cs
+++ Serveur/Model/Village.cs
@@ -3,8 +3,8 @@
     public class Village
     {
         public int IdUnivers { get; set; }
-        public String Faction { get; set; }
-        public String Name { get; set; }
+        public String Faction { get; set; } = "";
+        public String Name { get; set; } = "";
 
     }
 }
