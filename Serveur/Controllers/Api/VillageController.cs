0933fe4d334f3e2309a6cc88aa04844f1d213168
Pires "Indorie" Lucas <94078903+IndorieSama@users.noreply.github.com>
2022-12-15 15:27:58 +0100

    Requête Ressource + Validation

--- Serveur/Controllers/Api/VillageController.cs
+++ Serveur/Controllers/Api/VillageController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Mvc;
+using MySqlX.XDevAPI.Common;
+using System.Text.Json;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Server.Auth;
 using Server.Database;
@@ -139,5 +142,43 @@ namespace Server.Controllers.Api
                 ContentType = "application/json; charset=UTF-8",
             };
         }
+
+
+        /// <summary>
+        /// Get
+        /// </summary>
+        /// <param name="idVill"></param>
+        /// <returns></returns>
+        [HttpGet("{idVill}/resources/get")]
+        public async Task<IActionResult> GetRessources(int idVill)
+        {
+            int[] result = await Database.VillageDB.GetRessource(idVill);
+
+            return new ContentResult
+            {
+                Content = JsonSerializer.Serialize(result),
+                ContentType = "application/json; charset=UTF-8",
+            };
+        }
+
+        /// <summary>
+        /// Send
+        /// </summary>
+        /// <param name="idVill"></param>
+        /// <param name="gold"></param>
+        /// <param name="wood"></param>
+        /// <param name="stone"></param>
+        /// <returns></returns>
+        [HttpPost("{idVill}/resources/set")]
+        public async Task<IActionResult> SetRessources(int idVill, int gold, int wood, int stone)
+        {
+            bool result = await Database.VillageDB.UpdateRessources(idVill, wood, stone, gold);
+
+            return new ContentResult
+            {
+                Content = JsonSerializer.Serialize(result),
+                ContentType = "application/json; charset=UTF-8",
+            };
+        }        
     }
 }
